fe7009330b127d821d6d1e20617549cd98403615
Alexalom99 <alexalom1999@gmail.com>
2025-03-31 20:33:21 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // ¡Buena suerte!
+
+const bill = document.querySelector("#bill");
+const tip = document.querySelector("#tip");
+const button = document.querySelector("#calculate");
+const total = document.querySelector("#total");
+
+button.addEventListener("click", function (e) {
+    let result = parseFloat(bill.value) + parseFloat(bill.value)*parseFloat(tip.value)*0.01;
+    total.textContent = "$" + result.toFixed(2);
+});
